90b048fd908b95d87343e4e5a9c4f420f9fdee9a
temevh <temehama@gmail.com>
2024-01-06 11:33:21 +0200

    tilator init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Tilator</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
