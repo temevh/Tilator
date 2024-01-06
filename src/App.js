bb3fbd09dcc472ebae50bb99ed43137ce9e39620
temevh <temehama@gmail.com>
2024-01-06 11:53:17 +0200

    added MUI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Slider } from "@mui/material";
+
 function App() {
   return (
     <div className="App">
       <div className="bg-gray-500">
-        <p>Hello world</p>
+        <Slider
+          aria-label="Temperature"
+          defaultValue={30}
+          valueLabelDisplay="auto"
+          step={10}
+          marks
+          min={10}
+          max={110}
+        />
+        <Slider defaultValue={30} step={10} marks min={10} max={110} disabled />
       </div>
     </div>
   );
