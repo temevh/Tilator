0b4977201a4911656b36727f5c97c61b2f160734
temevh <temehama@gmail.com>
2024-01-11 16:17:59 +0200

    removed unnecessary configurations, disabled strict mode

--- frontend/src/components/leaderboard/Leaderboard.jsx
+++ frontend/src/components/leaderboard/Leaderboard.jsx
@@ -1,8 +1,14 @@
 import getLeaderboardData from "../API/getLeaderboardData";
 import { Button } from "@mui/material";
+import { useEffect } from "react";
 
 const Leaderboard = () => {
   let leaderboardData;
+
+  useEffect(() => {
+    console.log("Fetched leaderboard data");
+    fetchLeaderboard();
+  });
 
   const fetchLeaderboard = async () => {
     leaderboardData = await getLeaderboardData();
@@ -13,7 +19,12 @@ const Leaderboard = () => {
 
   function refreshLeaderboard() {}
 
-  return <Button onClick={fetchLeaderboard}>Leaderboard get</Button>;
+  return (
+    <>
+      <Button onClick={fetchLeaderboard}>Leaderboard get</Button>;
+      <p>{leaderboardData}</p>
+    </>
+  );
 };
 
 export default Leaderboard;
