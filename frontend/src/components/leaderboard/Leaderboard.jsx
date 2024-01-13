9889f29c97a251c5e775b983ac2d464082631080
temevh <temehama@gmail.com>
2024-01-13 13:45:04 +0200

    started to work on the leaderboard visualization

--- frontend/src/components/leaderboard/Leaderboard.jsx
+++ frontend/src/components/leaderboard/Leaderboard.jsx
@@ -1,33 +1,53 @@
-import getLeaderboardData from "../API/getLeaderboardData";
+import React, { useState } from "react";
 import { Button } from "@mui/material";
-import { useState } from "react";
+import getLeaderboardData from "../API/getLeaderboardData";
 
 const Leaderboard = () => {
   const [leaderboardData, setLeaderboardData] = useState([]);
+  const [mostCope, setMostCope] = useState("");
 
   const fetchLeaderboard = async () => {
     try {
       const data = await getLeaderboardData();
       console.log(data);
 
-      setLeaderboardData(data.map((item) => item.value));
+      setLeaderboardData(data);
     } catch (error) {
-      console.error("Error fetching leaderboar data:", error);
+      console.error("Error fetching leaderboard data:", error);
     }
   };
+
+  const calculateRankings = () => {
+    const sortedData = [...leaderboardData].sort((a, b) => b.cope - a.cope);
+    const rankings = {};
+
+    sortedData.forEach((item, index) => {
+      console.log(
+        index > 0 ? rankings[sortedData[index - 1].value.person] : null
+      );
+    });
+
+    return rankings;
+  };
+
+  const rankings = calculateRankings();
 
   return (
     <div>
       {leaderboardData.length === 0 ? (
         <p style={{ color: "white" }}>No people</p>
       ) : (
-        leaderboardData.map((item, index) => (
-          <div key={index} style={{ backgroundColor: "white" }}>
-            <p
-              style={{ color: "black" }}
-            >{`Person: ${item.person}, Cope: ${item.cope}, Tila: ${item.tila}, Bojoing: ${item.bojoing}, Sekavuus: ${item.sekavuus}, Turvotus: ${item.turvotus}`}</p>
-          </div>
-        ))
+        <div>
+          <h2 style={{ color: "white" }}>Leaderboard</h2>
+          <ol>
+            {leaderboardData.map((item, index) => (
+              <li key={index} style={{ color: "white" }}>
+                <strong style={{ color: "white" }}>{item.person}</strong> -
+                Cope: {item.cope} - Rank: {rankings[item.person]}
+              </li>
+            ))}
+          </ol>
+        </div>
       )}
       <Button onClick={fetchLeaderboard}>Leaderboard get</Button>
     </div>
